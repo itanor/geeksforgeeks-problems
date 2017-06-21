b4731bffe18a913cb7119c94806558add1bf56a9
Itanor Strapazzon <itanor@gmail.com>
2017-06-20 22:17:41 -0300

    using short circuit now

--- basic/arranging-the-array.js
+++ basic/arranging-the-array.js
@@ -7,8 +7,8 @@
 var v = [9, -6, 1, -1, 4, -8], result = [];
 
 for(var i = 0, len = v.length, j = v.length-1; i < len, j >= 0; i++, j--) {
-  if(v[i] >= 0) result.push(v[i]); 
-  if(v[j] < 0) result.unshift(v[j]);
+  v[i] >= 0 && result.push(v[i]); 
+  v[j] < 0 && result.unshift(v[j]);
 }
 
 console.log(result);
